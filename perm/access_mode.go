98bbf78676074908e44e1ced33437bed226f46fc
lizexin <546973743@qq.com>
2023-11-26 19:28:05 +0800

    first commit

--- perm/access_mode.go
+++ perm/access_mode.go
@@ -1,8 +1,15 @@
 package perm
 
+import (
+	"fmt"
+)
+
+// AccessMode specifies the users access mode
+type AccessMode int
+
 const (
 	// AccessModeNone no access
-	AccessModeNone int = iota // 0
+	AccessModeNone AccessMode = iota // 0
 	// AccessModeRead read access
 	AccessModeRead // 1
 	// AccessModeWrite write access
@@ -12,3 +19,36 @@ const (
 	// AccessModeOwner owner access
 	AccessModeOwner // 4
 )
+
+func (mode AccessMode) String() string {
+	switch mode {
+	case AccessModeRead:
+		return "read"
+	case AccessModeWrite:
+		return "write"
+	case AccessModeAdmin:
+		return "admin"
+	case AccessModeOwner:
+		return "owner"
+	default:
+		return "none"
+	}
+}
+
+func (mode AccessMode) LogString() string {
+	return fmt.Sprintf("<AccessMode:%d:%s>", mode, mode.String())
+}
+
+// ParseAccessMode returns corresponding access mode to given permission string.
+func ParseAccessMode(permission string) AccessMode {
+	switch permission {
+	case "read":
+		return AccessModeRead
+	case "write":
+		return AccessModeWrite
+	case "admin":
+		return AccessModeAdmin
+	default:
+		return AccessModeNone
+	}
+}
